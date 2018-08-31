2a78fbe78154e05b84aa6e5a366cb6fca831b356
Steven Pears <steven.pears@live.co.uk>
2018-08-31 08:09:44 +0100

    Add directive assertions (#4)

---
+++ Alexa.NET.Assertions/AmbiguousDirectiveException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Alexa.NET.Assertions
+{
+    public class AmbiguousDirectiveException : Exception
+    {
+        public AmbiguousDirectiveException(string message):base(message)
+        {
+            
+        }
+    }
+}
